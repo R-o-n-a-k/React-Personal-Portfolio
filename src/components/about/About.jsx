f09d70d813304a0bf1c831c371bcc1113afc22c1
Ronak <rroonnaakk7@gmail.com>
2024-03-13 01:02:21 +0530

    adding about section ( intro part)

---
+++ src/components/about/About.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <section className="about section" id="about">
+      <div className="about__section">
+        <div className="about__content">
+          <div className="about__greet">
+            <div className="about__intro__group">
+              <h3 className="about__greeting">- Nice to Meet you !</h3>
+              <h2 className="about__name"> Ronak Patel</h2>
+            </div>
+            <a href="#contactUs" className="button button--flex second__button">
+              Got a Project ?
+            </a>
+          </div>
+          <p className="about__intro">
+            A dedicated & passionate programmer with a zeal for knowledge.
+            <br />
+            <br />I thrive on exploring new technologies and deeply committed to
+            solving complex problems and making every challenge an opportunity
+            to grow and excel.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
