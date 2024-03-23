3e51582333c0fe9cd7b5985dc33fa2cb2941599f
Ronak <rroonnaakk7@gmail.com>
2024-03-24 01:08:22 +0530

    feat: adding contact empty section

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <div className="section contact" id="contact">
+      <h2 className="section__title">Contact</h2>
+    </div>
+  );
+}
+
+export default Contact;
