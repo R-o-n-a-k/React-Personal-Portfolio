dde398cbe77f1af95efcf307c33f0216ae643a60
Ronak Patel <70361182+R-o-n-a-k@users.noreply.github.com>
2024-04-03 12:55:08 +0530

    chore: removing extra imports

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import MyResume from "../../assets/Ronak.pdf";
 import TextTransitionLoop from './TextTransitionLoop';
 
