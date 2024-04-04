13f52dc424eb4a1be759179a73f7ac43632003fa
Ronak <rroonnaakk7@gmail.com>
2024-04-04 21:29:56 +0530

    feat: adding modal to the project section

--- src/components/projects/ProjectItems.jsx
+++ src/components/projects/ProjectItems.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
+import ProjectModal from "./ProjectModal";
 
 const ProjectItems = ({ item }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    document.body.classList.add("modal-open");
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    document.body.classList.remove("modal-open");
+  };
+
   return (
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
       <h3 className="work__title">{item.title}</h3>
-      <a href={item.ref} className="work__button" target="_blank">
-        Check out <i className="fa-solid fa-arrow-right work__button-icon"></i>
+      <a className="work__button" target="_blank" onClick={openModal}>
+        Show Details
+        <i className="fa-solid fa-arrow-right work__button-icon"></i>
       </a>
+      {isModalOpen && <ProjectModal item={item} closeModal={closeModal} />}
     </div>
   );
 };
---
+++ src/components/projects/ProjectModal.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./ProjectModal.css";
+
+const ProjectModal = ({ item, closeModal }) => {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <a className="modal-close" onClick={closeModal}>
+          <i className="fa-solid fa-times"></i>
+        </a>
+        <h3>{item.title}</h3>
+        <p>{item.desc}</p>
+
+        <a href={item.ref} target="_blank" rel="noopener noreferrer">
+          <button className="check-out" role="link">
+            <span className="circle" aria-hidden="true">
+              <span className="icon arrow"></span>
+            </span>
+            <span className="button-text">Check out</span>
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectModal;
