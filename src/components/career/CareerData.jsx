4d7106e4ab7b85664cbed1572e1918940e0409d2
Ronak Patel <70361182+R-o-n-a-k@users.noreply.github.com>
2024-04-08 13:19:46 +0530

    css: changing accordion color & adding highlight.

--- src/components/career/CareerData.jsx
+++ src/components/career/CareerData.jsx
@@ -22,10 +22,10 @@ export const workData = [
     company: "Capgemini",
     year: "Aug 2022 - Current",
     data: [
-      "Utilized JavaScript for project development, incorporating HTML and CSS, and applied basic React for enhanced functionality.",
-      "Implemented Playwright using TypeScript, developing scripts for test scenarios and introducing innovative workflows to enhance the project framework.",
-      " Led training sessions, fostering a dynamic learning environment for skill development and promoting effective project collaboration within the team.",
-      "Proficient in Java and Selenium automation testing, utilizing Maven and the Page Object Model for scalable frameworks. Expert in generating Screenshots and Reports with Extent, TestNG, and BDD. Skilled in crafting complex SQL queries for ETL testing to ensure data integrity across multiple sources.",
+      ["Utilized JavaScript for project development, incorporating",<span className="highlight"> HTML,CSS</span>," and applied basic",<span className="highlight"> React </span>,"for enhanced functionality."],
+      ["Implemented ",<span className="highlight"> Playwright</span>," using",<span className="highlight"> JavaScript</span>," , developing scripts for test scenarios and introducing innovative workflows to enhance the project framework."],
+      ["Led ",<span className="highlight">training sessions</span>,", fostering a dynamic learning environment for skill development and promoting effective project collaboration within the team."],
+      ["Proficient in ",<span className="highlight">Java</span>," and ",<span className="highlight">Selenium automation testing </span>," , utilizing Maven and the Page Object Model for scalable frameworks. Expert in generating Screenshots and Reports with ",<span className="highlight">Extent, TestNG,</span>," and ",<span className="highlight">BDD</span>," . Skilled in crafting complex ",<span className="highlight">SQL</span>," queries for ",<span className="highlight">ETL testing </span>," to ensure data integrity across multiple sources."],
     ],
   },
   {
@@ -33,9 +33,9 @@ export const workData = [
     company: "Techq Konnect",
     year: "Aug 2021 - Sept 2021",
     data: [
-      " Skilled in developing, optimizing, and maintaining websites using HTML, CSS, and JavaScript to meet client needs and goals.",
-      "Proficient in creating responsive and user-friendly web interfaces, adhering to best practices for web design and development.",
-      "Collaborated effectively with fellow interns as a team to deliver assigned projects ahead of schedule.",
+      [" Skilled in developing, optimizing, and maintaining websites using ",<span className="highlight">HTML, CSS,</span>," and ",<span className="highlight">JavaScript</span>," to meet client needs and goals."],
+      ["Proficient in creating ",<span className="highlight">responsive </span>," and ",<span className="highlight">user-friendly web interfaces</span>," adhering to best practices for web design and development."],
+      [<span className="highlight">Collaborated </span>," effectively with fellow interns as a team to deliver assigned projects ahead of schedule."],
     ],
   },
   {
@@ -43,9 +43,9 @@ export const workData = [
     company: "Sahu Technologies",
     year: "Jun 2021 - Jul 2021",
     data: [
-      " Worked with the fellow interns to meet project requirements, desired functionality by creating front-end website using HTML, CSS & JS.",
-      "Implemented enhancements that improved web functionality and responsiveness.",
-      "Learned the new subject and created special Handouts, study guides and assessments to evaluate and boost student knowledge.",
+      ["Worked with the fellow interns to meet project requirements, desired functionality by creating front-end website using ",<span className="highlight">HTML, CSS & JS</span>,"."],
+      ["Implemented enhancements that improved ",<span className="highlight">web functionality</span>," and ",<span className="highlight">responsiveness</span>,"."],
+      ["Learned the new subject and created special Handouts, study guides and assessments to evaluate and boost student knowledge."],
     ],
   },
 ];
